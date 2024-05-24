60f2b720d6e85817be1bc0bfd6070a49320c96e8
mmirazz <mehedimiraz900@gmail.com>
2024-05-24 18:38:14 +0600

    post job added

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const app =express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 
-const corsOptions ={
-    origin:['http://localhost:5173', 'http://localhost:5174'], 
-    credentials:true, 
-    optionSuccessStatus: 200
-}
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.6w72r5l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6w72r5l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,19 +24,55 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
-//Ex100iczxlh9a9RJ
-//jobHive
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const jobsCollection = client.db("jobHive").collection("jobs");
+
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+      // console.log(result);
+      // console.log(id);
+    });
+
+    app.post("/jobs", async (req, res) => {
+      const jobData = req.body;
+      const result = await jobsCollection.insertOne(jobData);
+      res.send(result);
+    });
+
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // app.get("/jobs/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   const query = { 'buyer?.email': email };
+    //   const result = await jobsCollection.find(query).toArray();
+    //   res.send(result);
+    // });
+
+
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -47,12 +80,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("Job hive making server is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`server is running on port : ${port}`);
-  });
+  res.send("Job hive making server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`);
+});
+
+//   console.log(run());
